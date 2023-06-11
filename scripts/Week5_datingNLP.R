77f51d0e30fb04ab39b125c8088bc851529345b0
Shmuel Amir <shmulik5777@gmail.com>
2023-06-11 21:50:56 +0300

    pca plots

--- scripts/Week5_datingNLP.R
+++ scripts/Week5_datingNLP.R
@@ -4,14 +4,13 @@ library(quanteda)
 library(Matrix)
 library(caret)
 library(rpart)
+library(ggplot2)
 
-# library(ggplot2)
 # library(mosaic)
 # library(xtable)
 # library(gridExtra)
 # library(stopwords)
 
-# pdf()
 options(digits = 2)
 
 # ---------- Load texts ----------#
@@ -26,8 +25,9 @@ essays <- apply(essays, MARGIN = 1, FUN = paste, collapse=" ")
 
 profiles_sex <- profiles$sex
 N <- length(profiles_sex)
+
 rm(profiles)
-  
+
 html <- c( "<a[^>]+>", "class=[\"'][^\"']+[\"']", "&[a-z]+;", "\n", "\\n", "<br ?/>", "</[a-z]+ ?>" )
 stop.words <- c( "a", "am", "an", "and", "as", "at", "are", "be", "but", "can", "do", "for", "have", "i'm", "if", "in", "is", "it", "like", "love", "my", "of", "on", "or", "so", "that", "the", "to", "with", "you", "i" )
 
@@ -68,19 +68,7 @@ tf <- prop.table(tokens.matrix, margin = 1)
 
 idf <- apply(tokens.matrix, MARGIN = 2, function(x) log10(dim(tokens.matrix)[1] / sum(x != 0)))
 
-tf.idf <- tf * idf
-# tf.idf <- na.omit(tf.idf)
-
-
-# quanteda.tf <- dfm_weight(dfm.trimmed[1:n_max, 1:n_max], "prop")
-# quanteda.idf <- docfreq(dfm.trimmed[1:n_max, 1:n_max], "inverse")
-# 
-# quanteda.df.idf <- quanteda.tf * quanteda.idf
-# all.equal(tf.idf, convert(quanteda.df.idf, to = "matrix"))
-# 
-# 
-# # quanteda implementation of tf idf (because mine not run on all data)
-# all.tokens.tfidf <- dfm_weight(dfm.trimmed, "prop") * docfreq(dfm.trimmed, "inverse")
+train.dfm <- tf * idf
 
 rm(tokens.matrix, tf, idf)
 gc()
@@ -88,10 +76,10 @@ gc()
 # ---------- male vs female by text ----------#
 
 # df, names
-all.tokens.df <- as.data.frame(tf.idf, row.names = NULL, optional = FALSE, make.names = TRUE)
-# all.tokens.df <- as.data.frame(dfm.trimmed, row.names = NULL, optional = FALSE, make.names = TRUE)
+all.tokens.df <- as.data.frame(train.dfm, row.names = NULL, optional = FALSE, make.names = TRUE)
+
 names(all.tokens.df) <- make.names(names(all.tokens.df), unique = TRUE) 
-all.tokens.df <- na.omit(all.tokens.df)
+
 rm(dfm.trimmed)
 gc()
 
@@ -100,46 +88,75 @@ cross_validation <- trainControl(method = "repeatedcv", number = 10, repeats = 3
 
 # male, female labels
 all.tokens.df <- cbind(sex = profiles_sex, data = all.tokens.df)
+all.tokens.df <- na.omit(all.tokens.df)
 rm(profiles_sex)
 
 # decision tree
-trainmodel <- train(sex ~ ., data = all.tokens.df, trControl = cross_validation, method = "rpart")
+before <- Sys.time() # how to do for each repeat??????????
+train.model <- train(sex ~ ., data = all.tokens.df, trControl = cross_validation, method = "rpart")
+Sys.time() - before
 
 # confusion matrix
-confusionMatrix(trainmodel) #rpart
+confusionMatrix(train.model) #rpart
+
 
 # ---------- Remove batch effect ----------#
-most_importate_words <- substring(names(trainmodel$finalModel$variable.importance), 6) # maybe without removing 6
+male.df <- subset(all.tokens.df, sex == "m")
+female.df <- subset(all.tokens.df, sex == "f")
 
-tf.idf <- tf.idf[, !colnames(tf.idf) %in% most_importate_words]
-tf.idf <- na.omit(tf.idf)
+male_term_freq <- colSums(male.df[, -1])
+female_term_freq <- colSums(female.df[, -1])
+
+num_most <- 25
+male_most_freq <- sort(male_term_freq, decreasing = TRUE)[1:num_most]
+female_most_freq <- sort(female_term_freq, decreasing = TRUE)[1:num_most]
+
+male_most_freq
+female_most_freq
+
+train.dfm <- train.dfm[, !colnames(train.dfm) %in% male_most_freq]
+train.dfm <- train.dfm[, !colnames(train.dfm) %in% female_most_freq]
+
+train.dfm <- na.omit(train.dfm)
 
 # ---------- Clustering ----------#
 
 # Calculate PCA
-pca <- prcomp(tf.idf)
-pca.data_frame <- data.frame(Text = rownames(pca$x), x = pca$x[,1], y = pca$x[,2])
-pca.var <- pca$sdev ^ 2
+cluster.pca <- prcomp(train.dfm)
+pca.data_frame <- data.frame(Text = rownames(cluster.pca$x), x = cluster.pca$x[,1], y = cluster.pca$x[,2])
+pca.var <- cluster.pca$sdev ^ 2
 pca.var.per <- round(pca.var / sum(pca.var) * 100, 1)
 
 colors <- paste0("cluster #", 1:10)
 
-# plot T-SNE / PCA for each cluster result
-for (k in c(2, 3, 4, 10)) {
+# plot PCA for each cluster result
+
+pdf("Week5_datingNLP.pdf")
+
+kmeans_plots <- function(k) {
+  clusters <- kmeans(train.dfm, k)
   
-  clusters <- kmeans(tf.idf, k)
-  
-  print(ggplot(data = pca.data, aes(x = X, y = Y, label = Sample ,colour = colors[clusters$cluster])) +
-          geom_point() + theme_bw() + ggtitle(paste0("Kmeans: K = ", k)) +
-          theme(legend.position = "bottom", panel.background = element_rect(fill = "grey")) +
-          xlab(paste0("PC1 (", pca.var.per[1], "%)")) + ylab(paste0("PC2 (", pca.var.per[2], "%)")))
+  ggplot(
+    data = pca.data_frame,
+    aes(x = x, y = y, label = Text, color = clusters$cluster)) +
+    geom_point() +
+    ggtitle(paste0("Kmeans: K = ", k, " clusters")) +
+    xlab(paste0("PC1 (", pca.var.per[1], "%)")) + 
+    ylab(paste0("PC2 (", pca.var.per[2], "%)")
+    )
 }
 
+kmeans_plots(2)
+kmeans_plots(3)
+kmeans_plots(4)
+kmeans_plots(10)
 
+# plot T-SNE
+sign.pca <- pca.var.per[pca.var.per > 0.7]
+tsne.df <- data.frame(pc = seq(1:length(sign.pca)), val = sign.pca)
+ggplot(data = tsne.df, aes(pc, val)) + 
+  geom_col()
 
+save(file="Week5_datingNLP.rdata", train.model, train.dfm, cluster.pca)
 
-
-
-
-
-
+dev.off()
